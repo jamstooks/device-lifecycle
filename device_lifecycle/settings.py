d215aa631b8de2f358d58067d17f83352883c33a
Benjamin Stookey <ben.stookey@gmail.com>
2016-11-27 23:06:31 -0700

    Open up allowed_hosts;

--- device_lifecycle/settings.py
+++ device_lifecycle/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'blahblahblah')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', None)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ('*',)
 
 
 # Application definition
