6a2968a99dfe460cd40e8d112e79e6570bcc9d43
Benjamin Stookey <ben.stookey@gmail.com>
2017-01-01 14:30:47 -0700

    Updated to latest whitenoise compression config;

--- device_lifecycle/settings.py
+++ device_lifecycle/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files, served with whitenoise
 STATIC_ROOT = 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_DIR, 'static'),
